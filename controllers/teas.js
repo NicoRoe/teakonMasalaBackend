b1e11f530b64637e1d5a9bdfe565e9291c1c7e44
vet <vetefir@gmail.com>
2023-04-28 17:20:23 +0200

    tee-object deteils joins - test - doppelte data

--- controllers/teas.js
+++ controllers/teas.js
@@ -21,8 +21,49 @@ const getTeeArten = async (req, res) => {
 			FROM tee 
 			JOIN join_tee_anbaugebiete ON tee.id = join_tee_anbaugebiete.tee_id 
 			JOIN anbaugebiete ON anbaugebiete.id = join_tee_anbaugebiete.anbaugebiet_id 
-			WHERE anbaugebiete.id = $1`, [anbaugebiete_id]);
+			WHERE anbaugebiete.id = $1`, [anbaugebiete_id] );
 		console.log('teesInAnbaugebietArray', teesInAnbaugebietArray.rows);
+
+			
+
+// https://www.postgresqltutorial.com/postgresql-tutorial/postgresql-inner-join/
+		
+	const tee_id =  2;
+	const teeAllDeteils = await pool.query(`
+	SELECT 
+		 
+		tee.id AS teeid, 
+		tee.name AS teename, 
+
+		anbaugebiete.id AS anbaugebieteid,
+		anbaugebiete.name AS anbaugebietename,
+
+		nebenwirkungen.id AS nebenwirkungenid,
+		nebenwirkungen.name AS nebenwirkungenname,
+
+		aromen.id AS aromenid,
+		aromen.name AS aromenname
+
+		FROM tee
+
+		  JOIN join_tee_anbaugebiete ON join_tee_anbaugebiete.tee_id = tee.id 
+		  JOIN anbaugebiete ON anbaugebiete.id = join_tee_anbaugebiete.anbaugebiet_id 
+
+		  JOIN join_tee_benefits ON tee.id = join_tee_benefits.tee_id 
+		  JOIN benefits ON benefits.id = join_tee_benefits.benefit_id 
+
+		  JOIN join_tee_nebenwirkungen ON tee.id = join_tee_nebenwirkungen.tee_id 
+		  JOIN nebenwirkungen ON nebenwirkungen.id = join_tee_nebenwirkungen.nebenwirkung_id 
+
+		  JOIN join_tee_aromen ON tee.id = join_tee_aromen.tee_id 
+		  JOIN aromen ON aromen.id = join_tee_aromen.aroma_id 
+
+
+
+		WHERE tee.id = $1`, [tee_id] );
+		console.log('teeAllDeteils', teeAllDeteils.rows);
+
+
 
 
 		res.json( { 
@@ -30,6 +71,7 @@ const getTeeArten = async (req, res) => {
 			teesArray: teesArray.rows,
 			anbaugebieteArray: anbaugebieteArray.rows,
 			teesInAnbaugebietArray: teesInAnbaugebietArray.rows,
+			teeAllDeteils: teeAllDeteils.rows,
 		} );
 
 		// res.json( [teeArtenArray.rows, teesArray.rows] );
